5eb3fdacb143c7e24a76ee5dd8fa0e6487041d92
MehdiIslam <mehdi.islam@epfl.ch>
2026-03-26 14:44:13 +0100

    install

---
+++ project_installation/example.py
@@ -0,0 +1,11 @@
+import matplotlib
+matplotlib.use("Agg")
+from cmc_project_pack.simulate import example_single
+
+if __name__ == "__main__":
+    print("Running example single simulation...")
+    example_single(
+        arena='ground',
+        fast=False,
+        headless=False
+    )
